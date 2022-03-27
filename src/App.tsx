77cbbce41d38d00957cfec5d1de8d4dffa5b8647
prachi2794 <101655519+prachi2794@users.noreply.github.com>
2022-03-28 02:59:09 +0530

    upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ProductList } from './Components/ProductList';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <ProductList/>     
+    </div>
+  );
+}
+
+export default App;
